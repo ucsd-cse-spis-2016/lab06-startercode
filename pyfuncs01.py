06991e8cb8c8eab48a59e8fc4d843981f0acd842
Phill Conrad <pconrad@cs.ucsb.edu>
2016-08-13 22:38:53 -0700

    initial starter code

---
+++ pyfuncs01.py
@@ -0,0 +1,15 @@
+# pyfuncs01.py
+# SPIS 2016 -- stubs for some sample functions
+
+def perimRect(length,width):
+   ''' Compute perimiter of rectangle, given length and width '''
+
+   return -999.99 # stub  @@@ replace this stub with the correct code @@@
+
+
+def areaRect(length,width):
+   ''' Compute area of rectangle, given length and width '''
+
+   return -999.99 # stub  @@@ replace this stub with the correct code @@@
+
+
